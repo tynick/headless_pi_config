2718ce235d4328dbdf3f4ce0b727b7fde628e5ce
tynick <ballerstat@gmail.com>
2017-10-12 20:32:20 -0600

    ejects sd card when complete. could use some error handling

--- configure_pi.sh
+++ configure_pi.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# needs error handling in case there is nothing there
 card_path='/Volumes/boot'
 
 
 activate_ssh () {
     touch ${card_path}/ssh
+    echo -e "\nSSH has been configured.\n"
 }
 
 activate_wifi () {
@@ -23,9 +25,19 @@ network={
     psk="${psk}"
     }
 EOF
-done
 
+echo -e "\nWiFi network ${ssid} has been configured.\n"
+
+}
+
+# needs error handling
+eject_card () {
+     sd_card=$(df -h | grep /Volumes/boot | awk '{ print $1 }')
+     echo -e "\nEjecting ${sd_card}\n"
+     diskutil unmount ${sd_card}
+     echo -e "\nDone. You can now remove your SD card.\n"
 }
 
 activate_ssh
 activate_wifi
+eject_card
